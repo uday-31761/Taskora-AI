7a5135c8f476b9b3ae58294bc9ff9d6a75770ed5
Vura Hari Venkata Naga Jagannadha Uday Kiran <2200031761@kluniversity.in>
2026-05-06 18:42:38 +0530

    Update SecurityConfig.java

--- backend/src/main/java/com/etharaai/config/SecurityConfig.java
+++ backend/src/main/java/com/etharaai/config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000","https://taskora-ai-gamma.vercel.app/"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
