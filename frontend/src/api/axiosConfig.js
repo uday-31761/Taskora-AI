f3c46fb7c10dc52ee0965a6873e2c7feb3e172ea
Vura Hari Venkata Naga Jagannadha Uday Kiran <2200031761@kluniversity.in>
2026-05-06 14:18:45 +0530

    Refactor axios configuration to use environment variable

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,15 +1,43 @@
-import axios from 'axios';
+// import axios from 'axios';
+
+// const api = axios.create({
+//     baseURL: 'http://localhost:8080/api/',
+// });
+
+// api.interceptors.request.use((config) => {
+//     const token = localStorage.getItem('token');
+//     if (token) {
+//         config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+// });
+
+// export default api;
+
+
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api/',
+    baseURL: import.meta.env.VITE_API_URL,
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
 
-api.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
+// Attach JWT token automatically
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("token");
+
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
     }
-    return config;
-});
+);
 
 export default api;
